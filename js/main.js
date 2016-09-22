302356107bd6020e41c7aaa4003cef10654303c7
Pedro Castilho <cstl.px@gmail.com>
2016-09-22 00:05:50 -0300

    Reformulando o index.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
+"use strict"
+
 const CHART = document.getElementById("lineChart");
 
-
-var geju_data = null;
-var geju_json = null
-
-
+var filteredWords = [];
 
 function readJSON(url){
     var request = new XMLHttpRequest();
@@ -14,43 +12,82 @@ function readJSON(url){
         return request.responseText;
 };
 
-var geju_json = JSON.parse(readJSON("https://graph.facebook.com/geraldojulio40/posts/?since=2016-08-15&access_token=669914756492807|LwbpppgvMTljvj-QAFn-K1-3r7E&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1471491166&oauth_nonce=U5zDJL&oauth_version=1.0&oauth_signature=yt7Iic9uyOfiXvG3mZOe8q+EH00="));
-var coelho_json = JSON.parse(readJSON());
-var krause_json = JSON.parse(readJSON());
-var jp_json = JSON.parse(readJSON());
-var ed_json = JSON.parse(readJSON());
-var carlos_json = JSON.parse(readJSON());
-
-for (var i = 0; i < geju_json.length; i++) {
-  var msg_geju = geju_json.data[i].message;
-}
-
-function freqCount(jsonString) {
-   var rawWords = jsonString.split(" ");
-   var cleanWords = words.replace(/[{\"\':}]/g, "");
-   var words = cleanWords.filter(function(w) {
-      return w !== "";
+function freqCount(string,freqCounts) {
+   var rawWords = string.split(" ");
+   var words = rawWords.filter(function(w) {
+       return filteredWords.indexOf(w) < 0;
    });
 
-   var freqCounts = {};
-   for(index in words) {
-      if(!(words[index] in freqCounts)) {
-          freqCounts[words[index]] = 0;
-      } else {
-          freqCounts[words[index]]++;
-      }
-   }
+    for(var index in words) {
+       if(!(words[index] in freqCounts)) {
+           freqCounts[words[index]] = 1;
+       } else {
+           freqCounts[words[index]]++;
+       }
+    }
 
+    console.log(freqCounts);
    return freqCounts;
 }
 
+function sortedFreqs(freqs,candidate) {
+    var freqTable = freqs[candidate];
+
+    if(freqTable == undefined) {
+	return undefined;
+    } else {
+	var array = [];
+	
+	for(var w in freqTable) {
+	    array.push({ "word" : w, "frequency" : freqTable[w] });
+	}
+
+	var sorted = array.sort(function(a,b) {
+	    return a.frequency < b.frequency;
+	});
+
+	return sorted;
+    }
+}
+
+const pages = {   "geju"   : "geraldojulio40",
+		  "krause" : "priscilakrauseoficial",
+		  "jp"     : "",
+		  "ed"     : "EdilsonSilvaPSOL",
+		  "carlos" : ""
+	      };
 
 
+var jsons = {};
+for(i in pages) {
+    if(pages[i] != "") {
+	var queryStr = "https://graph.facebook.com/"+pages[i]+"/posts/?since=2016-08-15&access_token=669914756492807|LwbpppgvMTljvj-QAFn-K1-3r7E&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1471491166&oauth_nonce=U5zDJL&oauth_version=1.0&oauth_signature=yt7Iic9uyOfiXvG3mZOe8q+EH00="
+	jsons[i] = JSON.parse(readJSON(queryStr));
+    }
+}
 
+var counts = {};
+for(var i in jsons) {
+    console.log(i,":");
+    var jsonData = jsons[i];
+    console.log("data :",jsonData);
+    var freq = {}
+    for(var j = 0; j < jsonData.data.length; ++j) {
+	var msg = jsonData.data[j].message;
+	freq = freqCount(msg,freq);
+    }
 
+    counts[i] = freq;
+}
 
+//aqui, counts contém os objectos com as contagens de palavras para cada candidato
 
+//para obter a contagem ordenada para cada candidato, usa-se a função sortedFreqs com o identificador daquele candidato
+var sorted = sortedFreqs(counts,"geju");
 
+//essa função retorna um array de objetos da forma { "word" : palavra, "frequency" : contagem }
+
+/*
 let lineChart =  new Chart(CHART, {
     type: 'radar',
     data: {
@@ -102,4 +139,4 @@ let lineChart =  new Chart(CHART, {
         }
     ]
     }
-});
+});*/
